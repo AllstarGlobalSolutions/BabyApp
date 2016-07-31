f8e45f8c86c7d6edf911db492616b16c0b088ce0
Eric S. Hatfield <eric.hatfield@allstarglobalsolutions.com>
2016-07-31 17:46:07 +0800

    added button ono home page

--- BabyApp/BabyApp/App.cs
+++ BabyApp/BabyApp/App.cs
@@ -9,22 +9,43 @@ namespace BabyApp
 {
 	public class App : Application
 	{
+		public Button pushButton;
+
 		public App()
 		{
+			pushButton = new Button
+			{
+				HeightRequest = 160,
+				WidthRequest = 160,
+				Image = ( FileImageSource )FileImageSource.FromFile( "button-up.png" ),
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			pushButton.Clicked += PushButton_Clicked;
+
 			// The root page of your application
 			MainPage = new ContentPage
 			{
+				BackgroundColor = Color.Teal,
 				Content = new StackLayout
 				{
 					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					Padding = 20,
 					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
+						pushButton
 					}
 				}
 			};
+
+		}
+
+		private void PushButton_Clicked( object sender, EventArgs e )
+		{
+			Button button = ( Button )sender;
+			button.Image = ( FileImageSource )FileImageSource.FromFile( "button-pressed.png" );
+			// then go to new page
 		}
 
 		protected override void OnStart()
