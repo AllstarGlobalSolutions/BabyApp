3302ab3faf26ec72a9531cd7da1a721f34487830
Eric Hatfield <eric.hatfield@allstarglobalsolutions.com>
2016-08-04 13:57:19 +0800

    Added New Pages

---
+++ BabyApp/BabyApp/DonationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BabyApp
+{
+	public partial class DonationPage : ContentPage
+	{
+		public DonationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
