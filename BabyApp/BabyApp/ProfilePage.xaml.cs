698ff61b404d57da09c1f1427fa89379ecb70de6
Eric Hatfield <eric.hatfield@allstarglobalsolutions.com>
2016-08-04 12:37:27 +0800

    new page

--- BabyApp/BabyApp/ProfilePage.xaml.cs
+++ BabyApp/BabyApp/ProfilePage.xaml.cs
@@ -5,9 +5,9 @@ using Xamarin.Forms;
 
 namespace BabyApp
 {
-	public partial class SettingsPage : ContentPage
+	public partial class ProfilePage : ContentPage
 	{
-		public SettingsPage()
+		public ProfilePage()
 		{
 			InitializeComponent();
 		}
