c4e7f05ddfab5a70eb6b3bd3cec305315ee88972
Eric Hatfield <eric.hatfield@allstarglobalsolutions.com>
2016-08-08 13:23:19 +0800

    integrated new images for toolbar

--- BabyApp/BabyApp/NeedViewPage.xaml.cs
+++ BabyApp/BabyApp/NeedViewPage.xaml.cs
@@ -9,18 +9,18 @@ namespace BabyApp
 		public NeedViewPage()
 		{
 			InitializeComponent();
-//			adImage.Source = GetNextAdImageSource();
-//			needImage.Source = GetNextNeedImageSource();
+			adImage.Source = GetNextAdImageSource();
+			needImage.Source = GetNextNeedImageSource();
 		}
 
 		protected ImageSource GetNextAdImageSource()
 		{
-			return ImageSource.FromUri( new Uri( "" ) );
+			return ImageSource.FromUri( new Uri( "http://marketingforhippies.com/wp-content/uploads/2015/03/small-is-beautiful-banner.png" ) );
 		}
 
 		protected ImageSource GetNextNeedImageSource()
 		{
-			return ImageSource.FromUri( new Uri( "" ) );
+			return ImageSource.FromUri( new Uri( "http://www.bet.com/news/national/2016/06/09/tragic--mother-kills-her-small-children-while-on-family-vacation/_jcr_content/image.heroimage.dimg/__1465504128251/060916-national-tragic-mother-kills-her-small-children-while-on-family-vacation.jpg" ) );
 		}
 
 		public void OnImagePropertyChanged( object sender, PropertyChangedEventArgs args )
@@ -37,31 +37,31 @@ namespace BabyApp
 
 			switch ( toolbarItem.Text )
 			{
-				case "Details":
-					Navigation.PushAsync( new NeedDetailViewPage() );
-					break;
+			case "Details":
+				Navigation.PushAsync( new NeedDetailViewPage() );
+				break;
 
-				case "Org Info":
-					Navigation.PushAsync( new OrganizationViewPage() );
-					break;
+			case "Org Info":
+				Navigation.PushAsync( new OrganizationViewPage() );
+				break;
 
-				case "Donate":
-					Navigation.PushAsync( new DonationViewPage() );
-					break;
+			case "Donate":
+				Navigation.PushAsync( new DonationViewPage() );
+				break;
 
-				case "Save":
-					SaveNeed();
-					break;
+			case "Save":
+				SaveNeed();
+				break;
 
-				case "Saved Needs":
-					Navigation.PushAsync( new SavedNeedsViewPage() );
-					break;
+			case "Saved Needs":
+				Navigation.PushAsync( new SavedNeedsViewPage() );
+				break;
 			}
 		}
 
 		protected void SaveNeed()
 		{
-			throw new NotImplementedException();
+			//throw new NotImplementedException();
 		}
 	}
 }
