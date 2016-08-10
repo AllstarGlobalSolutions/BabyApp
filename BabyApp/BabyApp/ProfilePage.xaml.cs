59ccc5a731642c440df23badd3d92a7c1488ab24
Eric S. Hatfield <eric.hatfield@allstarglobalsolutions.com>
2016-08-11 03:21:13 +0800

    finally got registration to work

--- BabyApp/BabyApp/ProfilePage.xaml.cs
+++ BabyApp/BabyApp/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Xamarin.Forms;
 using BabyApp.Helpers;
 using BabyApp.ViewModels;
@@ -9,6 +10,7 @@ using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BabyApp
 {
@@ -16,11 +18,18 @@ namespace BabyApp
 	{
 		ProfileViewModel profileViewModel = ( ( App )Application.Current ).ProfileViewModel;
 
-		[DataContract]
+		[JsonObject( MemberSerialization.OptIn )]
 		class RegisterModel
 		{
+			[JsonProperty]
 			public string Email { get; set; }
+			[JsonProperty]
+			public string Surname { get; set; }
+			[JsonProperty]
+			public int Type { get; set; }
+			[JsonProperty]
 			public string Password { get; set; }
+			[JsonProperty]
 			public string ConfirmPassword { get; set; }
 		}
 
@@ -49,104 +58,44 @@ namespace BabyApp
 
 		public async void RegisterUser()
 		{
-			try
+			ErrorLabel.IsVisible = false;
+
+			using ( var client = new HttpClient() )
 			{
-				// Create a request using a URL that can receive a post. 
-				WebRequest request = WebRequest.Create( "http://192.168.1.6:3000/api/Account/Register" );
-				// Set the Method property of the request to POST.
-				request.Method = "POST";
-				// Create POST data and convert it to a byte array.
-				string postData = "Username=" + profileViewModel.Email + "&Password=Password@123&ConfirmPassword=Password@123";
-				byte[] byteArray = System.Text.Encoding.UTF8.GetBytes( postData );
-				// Set the ContentType property of the WebRequest.
-				request.ContentType = "application/x-www-form-urlencoded";
-				// Get the request stream.
-				Stream dataStream = await request.GetRequestStreamAsync();
-				// Write the data to the request stream.
-				dataStream.Write( byteArray, 0, byteArray.Length );
-				// Get the response.
-				WebResponse response = await request.GetResponseAsync();
-				// Display the status.
-				profileViewModel.GivenName = ( ( HttpWebResponse )response ).StatusDescription;
-				// Get the stream containing content returned by the server.
-				dataStream = response.GetResponseStream();
-				// Open the stream using a StreamReader for easy access.
-				StreamReader reader = new StreamReader( dataStream );
-				// Read the content.
-				string responseFromServer = reader.ReadToEnd();
+				try
+				{
+					client.DefaultRequestHeaders.Clear();
+					client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
+					RegisterModel rm = new RegisterModel
+					{
+						Email = profileViewModel.Email,
+						Surname = profileViewModel.Surname ?? "Anonymous",
+						Type = 2,
+						Password = "Password@123",
+						ConfirmPassword = "Password@123"
+					};
+					var str = JsonConvert.SerializeObject( rm );
+					HttpContent content = new StringContent( str , Encoding.UTF8, "application/json" );
+//					Uri uri = new Uri( "", UriKind.Absolute );
+					HttpResponseMessage response = await client.PostAsync( "http://192.168.1.6:3000/api/Account/Register", content );
+
+					if ( response.IsSuccessStatusCode )
+					{
+						SaveSettings();
+						Navigation.PopModalAsync();
+					}
+					else
+					{
+						ErrorLabel.Text = response.ReasonPhrase;
+						ErrorLabel.IsVisible = true;
+					}
+				}
+				catch ( Exception e )
+				{
+					ErrorLabel.Text = e.Message;
+					ErrorLabel.IsVisible = true;
+				}
 			}
-			catch ( WebException we )
-			{
-				profileViewModel.GivenName = we.Message;
-			}
-			/*
-			var uri = new Uri( "http://192.168.1.6:3000/api/Account/Register" );
-			RegisterModel rm = new RegisterModel
-			{
-				Email = profileViewModel.Email,
-				Password = "Password@123",
-				ConfirmPassword = "Password@123"
-			};
-
-			request = WebRequest.Create( uri );
-			request.
-			var serializer = new DataContractSerializer( typeof( RegisterModel ) );
-			var stream = new MemoryStream();
-			serializer.WriteObject( stream, rm );
-			var content = new StreamContent( stream );
-
-			HttpClient client = new HttpClient();
-			HttpResponseMessage response = await client.PostAsync( uri, content );
-			if ( response.IsSuccessStatusCode )
-			{
-				profileViewModel.GivenName = "success";
-			}
-			else
-			{
-				profileViewModel.GivenName = "failure";
-			}
-			*/
-			/*			var content = new StringContent( json, System.Text.Encoding.UTF8, "application/json" );
-
-						HttpResponseMessage response = null;
-						HttpClient client = new HttpClient();
-						response = await client.PostAsync( uri, content );
-
-						if ( response.IsSuccessStatusCode )
-						{
-							profileViewModel.GivenName = "Success";
-						}
-						*/
-
-			//			using ( var client = new HttpClient() )
-			//			{
-			/*
-			client.DefaultRequestHeaders.Accept.Clear();
-			client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue( "application/json" ) );
-			var serializer = new DataContractJsonSerializer( typeof( RegisterModel ) );
-			RegisterModel rm = new RegisterModel
-			{
-				Email = profileViewModel.Email,
-				Password = "Password@123",
-				ConfirmPassword = "Password@123"
-			};
-			var ms = new MemoryStream();
-			serializer.WriteObject( ms, rm );
-			HttpContent content = new StreamContent( ms );
-			Uri uri = new Uri( "http://192.168.1.6:3000/api/Account/Register", UriKind.Absolute );
-			HttpResponseMessage response = await client.PostAsync( uri, content );
-
-			if ( response.IsSuccessStatusCode )
-			{
-				// Save Settings locally
-				SaveSettings();
-				( ( App )Application.Current ).LoginAsync();
-
-				// pop this page and go to welcome page
-				await Navigation.PopModalAsync();
-			}
-			*/
-			//			}
 		}
 
 		void SaveSettings()
